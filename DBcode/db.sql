2476d20225cbcb094b6c53e2a9fe298b863dc815
np <nandanpatilm15@gmail.com>
2024-07-26 16:42:13 +0530

    added db.sql file containing MySQL queries

---
+++ DBcode/db.sql
@@ -0,0 +1,12 @@
+-- I have used MySQL database for this project
+-- the code is as follows
+
+create database collegeroommanagementdb; -- This query creates a new database "collegeroommanagementdb"
+
+use collegeroommanagementdb; -- to come to collegeroommanagementdb to create our tabels
+
+create table room(
+    int room_id;
+    name varchar
+)
+
--- src/db/DBConnection.java
+++ src/db/DBConnection.java
@@ -23,8 +23,8 @@ import java.sql.SQLException;
 
 public class DBConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/CollegeRoomManagementDB?serverTimezone=UTC";
-    private static final String USER = "root";
-    private static final String PASSWORD = "myROOTpass";
+    private static final String USER = "username";
+    private static final String PASSWORD = "password";
 
     static {
         try {
